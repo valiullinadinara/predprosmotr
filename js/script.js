cfd6f9b1a2cd08fe4af1d04ef8cc0a88aa43b500
valiullinadinara <dianadura>
2019-03-08 02:18:41 +0300

    4
    4

--- js/script.js
+++ js/script.js
@@ -1,15 +1,143 @@
-let s = 18473902;
+$( "#img1" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+$( "#img2" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+$( "#img3" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+$( "#img4" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+
+var s = 18473902;
 window.onload=function(){
 setInterval(function(){
-  s+=23820;
+  s+=27838;
   document.getElementById("sum").children[0].innerHTML=s;
 },1000)
 }
 
-$( "img2" ).on("mouseenter", function() {
-  $( "img2" ).animate({
-    left: "+=100",
-    top: "+=100"
-  }, 2000, function() {
-  });
-});
+
+
+// $(document).ready(function(){
+//   $("#menu").on("click","a", function (event) {
+//     event.preventDefault();
+//     var id  = $(this).attr('href'),
+//         top = $(id).offset().top;
+//     $('body,html').animate({scrollTop: top}, 1000);
+//   });
+// });
