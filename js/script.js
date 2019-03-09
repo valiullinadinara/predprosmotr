b0c96e03c7b61aa95cf449d1ede89f654a1a1b77
valiullinadinara <dianadura>
2019-03-09 23:49:04 +0300

    7
    7

--- js/script.js
+++ js/script.js
@@ -130,14 +130,3 @@ setInterval(function(){
   document.getElementById("sum").children[0].innerHTML=s;
 },1000)
 }
-
-
-
-// $(document).ready(function(){
-//   $("#menu").on("click","a", function (event) {
-//     event.preventDefault();
-//     var id  = $(this).attr('href'),
-//         top = $(id).offset().top;
-//     $('body,html').animate({scrollTop: top}, 1000);
-//   });
-// });
